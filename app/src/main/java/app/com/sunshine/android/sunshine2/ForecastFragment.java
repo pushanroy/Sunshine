266ae320393f48e8643ca2f2f7b617b15889aa3b
pushanroy <pushan87@gmail.com>
2016-11-28 19:05:03 +0530

    sunshine forecast fragment

---
+++ app/src/main/java/app/com/sunshine/android/sunshine2/ForecastFragment.java
@@ -0,0 +1,159 @@
+package app.com.sunshine.android.sunshine2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ForecastFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class ForecastFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    private ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] data = { "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"};
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        HttpsURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String forecastJsonStr = null;
+
+        try{
+            String baseUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+            String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+
+            URL url = new URL(baseUrl.concat(apiKey));
+
+            urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+
+            if(inputStream == null){
+                return null;
+            }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while((line  = reader.readLine()) != null){
+                buffer.append(line+"\n");
+            }
+
+            if(buffer == null){
+                return null;
+            }
+
+            forecastJsonStr = buffer.toString();
+
+        }catch (IOException e){
+            Log.e("MainFragment", "Error", e);
+            return null;
+        }finally {
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+
+            if(reader != null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+
+        }
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/app/com/sunshine/android/sunshine2/MainActivity.java
+++ app/src/main/java/app/com/sunshine/android/sunshine2/MainActivity.java
@@ -1,13 +1,19 @@
 package app.com.sunshine.android.sunshine2;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ForecastFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //TODO
     }
 }
