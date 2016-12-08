81c3e8b57ddc9aa43d6ba6bf41567d348448a93b
pushanroy <pushan87@gmail.com>
2016-12-08 19:44:30 +0530

    sunshine URI builder

--- app/src/main/java/app/com/sunshine/android/sunshine2/ForecastFragment.java
+++ app/src/main/java/app/com/sunshine/android/sunshine2/ForecastFragment.java
@@ -20,12 +20,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
 
 
 /**
@@ -59,6 +58,8 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
 
         if(id == R.id.action_refresh){
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute("94043");
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -131,23 +132,42 @@ public class ForecastFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>{
+    public class FetchWeatherTask extends AsyncTask<String, Void, Void>{
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
         @Override
-        protected Void doInBackground(Void... params) {
-            HttpsURLConnection urlConnection = null;
+        protected Void doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
             String forecastJsonStr = null;
+            String format = "json";
+            String units = "metric";
+            String numDays = "7";
 
             try{
-                String baseUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
-                String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+                final String APPID_PARAM = "APPID";
+                /*String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                String apiKey = "&appid=" + "afb2e5338cb3fd3724b48844326b8537";*/
 
-                URL url = new URL(baseUrl.concat(apiKey));
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM,numDays)
+                        .appendQueryParameter(APPID_PARAM, "afb2e5338cb3fd3724b48844326b8537")
+                        .build();
 
-                urlConnection = (HttpsURLConnection) url.openConnection();
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URL: "+builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
 
@@ -170,6 +190,8 @@ public class ForecastFragment extends Fragment {
                 }
 
                 forecastJsonStr = buffer.toString();
+                //forecastJsonStr = new String("{\"city\":{\"id\":1257325,\"name\":\"Santoshpur\",\"coord\":{\"lon\":88.264999,\"lat\":22.52528},\"country\":\"IN\",\"population\":0},\"cod\":\"200\",\"message\":0.0205,\"cnt\":7,\"list\":[{\"dt\":1481176800,\"temp\":{\"day\":30.47,\"min\":14.15,\"max\":30.47,\"night\":14.15,\"eve\":23.69,\"morn\":26},\"pressure\":1026.43,\"humidity\":70,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"speed\":3.86,\"deg\":358,\"clouds\":0},{\"dt\":1481263200,\"temp\":{\"day\":25.36,\"min\":12.84,\"max\":26.1,\"night\":13.7,\"eve\":20.84,\"morn\":12.84},\"pressure\":1024.04,\"humidity\":69,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"speed\":4.37,\"deg\":3,\"clouds\":0},{\"dt\":1481349600,\"temp\":{\"day\":24.68,\"min\":13.66,\"max\":25.54,\"night\":13.72,\"eve\":20.52,\"morn\":13.66},\"pressure\":1024.13,\"humidity\":67,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"speed\":4.8,\"deg\":4,\"clouds\":0},{\"dt\":1481436000,\"temp\":{\"day\":25.38,\"min\":13.86,\"max\":25.38,\"night\":19.13,\"eve\":23,\"morn\":13.86},\"pressure\":1022.92,\"humidity\":0,\"weather\":[{\"id\":802,\"main\":\"Clouds\",\"description\":\"scattered clouds\",\"icon\":\"03d\"}],\"speed\":2.14,\"deg\":354,\"clouds\":34},{\"dt\":1481522400,\"temp\":{\"day\":25.34,\"min\":18.77,\"max\":25.34,\"night\":19.98,\"eve\":24.16,\"morn\":18.77},\"pressure\":1022.22,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":2.08,\"deg\":19,\"clouds\":35},{\"dt\":1481608800,\"temp\":{\"day\":26.25,\"min\":18.13,\"max\":26.25,\"night\":19.07,\"eve\":24.05,\"morn\":18.13},\"pressure\":1021.5,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":1.17,\"deg\":67,\"clouds\":21,\"rain\":0.63},{\"dt\":1481695200,\"temp\":{\"day\":26.59,\"min\":16.63,\"max\":26.59,\"night\":17.55,\"eve\":23.33,\"morn\":16.63},\"pressure\":1023.42,\"humidity\":0,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"speed\":2.48,\"deg\":23,\"clouds\":1}]}");
+                Log.v(LOG_TAG, "Date Received: "+forecastJsonStr);
 
             }catch (IOException e){
                 Log.e("MainFragment", "Error", e);
